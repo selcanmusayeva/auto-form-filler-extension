d8d260c284a45ebd714af89faeaaa7271088bf69
Seljan <118933073+selcanmusayeva@users.noreply.github.com>
2024-11-19 17:55:21 +0400

    Edited Homepage

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const accountNameElement = document.getElementById("accountName");
+// document.addEventListener("DOMContentLoaded", () => {
+//     const accountNameElement = document.getElementById("accountName");
   
-    chrome.storage.local.get("accountName", (data) => {
-      if (data.accountName) {
-        accountNameElement.textContent = data.accountName;
-      } else {
-        accountNameElement.textContent = "Not found";
-      }
-    });
-  });
+//     chrome.storage.local.get("accountName", (data) => {
+//       if (data.accountName) {
+//         accountNameElement.textContent = data.accountName;
+//       } else {
+//         accountNameElement.textContent = "Not found";
+//       }
+//     });
+//   });
   
